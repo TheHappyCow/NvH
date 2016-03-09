20d9cd85395d5a1debaa8c6e42cdf9e4c468188c
Arjan Adriaanse <arjan@adriaan.se>
2016-03-09 13:24:43 +0000

    camera speed depends on game time

--- Core1/Camera.cs
+++ Core1/Camera.cs
@@ -6,25 +6,33 @@ using Microsoft.Xna.Framework;
 
 public class Camera : GameObject
 {
+    private int dx, dy;
+    private const int step = 500;
+
     public Camera(string id) : base(id) { }
 
     public override void HandleInput(InputHelper inputHelper)
     {
-        int dx = 0, dy = 0;
+        dx = 0;
+        dy = 0;
 
         if (inputHelper.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Left))
-            dx -= 1;
+            dx -= step;
 
         if (inputHelper.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Right))
-            dx += 1;
+            dx += step;
 
         if (inputHelper.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Up))
-            dy -= 1;
+            dy -= step;
 
         if (inputHelper.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Down))
-            dy += 1;
+            dy += step;
+    }
 
-        this.position.X += dx;
-        this.position.Y += dy;
+    public override void Update(GameTime gameTime)
+    {
+        this.position.X += dx * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        this.position.Y += dy * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        base.Update(gameTime);
     }
 }
