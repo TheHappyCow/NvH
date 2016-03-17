509eeb186f23e40123a4bb667e402e664b82015b
Timo van Milligen <cobravspanda01@gmail.com>
2016-03-17 19:54:51 +0100

    Added resourcecontroller class
    yes

---
+++ Main/Main/ResourceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+class ResourceController:GameObject
+    {
+
+    public ResourceController() : base("resourceController")
+    {
+
+    }
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+}
+
