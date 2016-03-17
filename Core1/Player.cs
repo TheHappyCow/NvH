61f0b30864c8c95188031173f843ff0513199037
Daan Paardekooper <daan24@gmail.com>
2016-03-17 17:22:40 +0100

    Added player class and change the cursor color/size

---
+++ Core1/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GoingBeyond5
+{
+    public class Player
+    {
+        internal GamePadState lastState;
+
+        internal Texture2D picture;
+
+        Random random = new Random();
+
+        public Player()
+        {
+            //instantiate resources
+        }
+
+        internal void Update(GameTime gameTime)
+        {
+            //handle resources
+        }
+    }
+}
